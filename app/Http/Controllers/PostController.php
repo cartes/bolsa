9b7dde11d454e9bd5c472b341c27d6ea29def3b5
cartes <cristiabncartesa@gmail.com>
2020-01-15 17:46:02 -0300

    Faltaba employees en Business Meta

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Business;
+use App\BusinessMeta;
 use App\Http\Requests\BusinessRegisterRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class PostController extends Controller
 {
@@ -12,7 +15,36 @@ class PostController extends Controller
         return view('posts.create');
     }
 
-    public function register(BusinessRegisterRequest $request) {
-        dd($request);
+    public function register(BusinessRegisterRequest $request)
+    {
+        $business = new Business;
+        $businessMeta = new BusinessMeta;
+
+        $business->rut_user = $request->rut;
+        $business->password = Hash::make($request->password);
+        $business->firstname = $request->firstname;
+        $business->surname = $request->surname;
+        $business->email = $request->email;
+        $business->phone = $request->phone;
+
+        $business->save();
+
+        $businessMeta->id_business = $business->id;
+        $businessMeta->rut_business = $request->rut;
+        $businessMeta->business_name = $request->business_name;
+        $businessMeta->activity = $request->activity;
+        $businessMeta->phone = $request->phone;
+        $businessMeta->employees = $request->employees;
+
+        $businessMeta->save();
+
+        session()->flush();
+        session([
+            'id' => $business->id,
+            'name' => $businessMeta->business_name,
+            'role' => 'Business'
+        ]);
+
+        return back()->with('message', ['success', 'Empresa Creada']);
     }
 }
