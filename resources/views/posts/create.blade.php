1139f09bc0acf61e1981a739fd80359889021cb8
cartes <cristiabncartesa@gmail.com>
2020-01-16 10:31:40 -0300

    Creación de nuevas oportunidades

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,9 +1,50 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="row justify-content-center">
-        <div class="col-md-10">
-            <h3 class="text-center">Publica un Aviso</h3>
+    <div class="container">
+        @if(session()->has('message'))
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="alert alert-{{session('message')[0]}}" role="alert">
+                        {{session('message')[1]}}
+                    </div>
+                </div>
+            </div>
+        @endif
+
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                @if(session()->get('id'))
+                    @include('partials.business.offer')
+                @else
+                    @include('partials.business.register')
+                @endif
+            </div>
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script src="{{ asset('js/tinymce/tinymce.min.js') }}"></script>
+
+    <script>
+        jQuery(document).ready(function ($) {
+            $(document).ready(function () {
+                tinymce.init({
+                    selector: '#contents',
+                    schema: 'html5',
+                    themes: 'modern',
+                    language: 'es_ES',
+                    menubar: false,
+                    plugins: "lists",
+                    skin: 'oxide',
+                    statusbar: false,
+                    toolbar: [
+                        'bold italic underline numlist bullist',
+                        'code'
+                    ],
+                });
+            });
+        });
+    </script>
+@endpush
