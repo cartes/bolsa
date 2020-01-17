f4414f0492ad8a4c1e72a5f0b7366fbaa4226e75
cartes <cristiabncartesa@gmail.com>
2020-01-17 10:26:42 -0300

    Controlador corregido

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Business;
 use App\Http\Requests\BusinessLoginRequest;
+use App\Http\Requests\CreateOfferRequest;
+use App\Offers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -33,5 +35,31 @@ class BusinessController extends Controller
         } else {
             return back()->with('message', ['warning', 'Email no está registrado como empresa']);
         }
+    }
+
+    public function createOffer(CreateOfferRequest $request)
+    {
+        $offer = new Offers;
+        $business_id = session()->get('id');
+
+        $offer->id_business = $business_id;
+        $offer->title = $request->title;
+        $offer->description = $request->description;
+        $offer->area = $request->area;
+        $offer->sub_area = $request->subarea;
+        $offer->country = $request->country;
+        $offer->state = $request->state;
+        $offer->city = $request->city;
+        $offer->comune = $request->comune;
+        $offer->address = $request->address;
+        $offer->salary = $request->salary;
+
+        $offer->save();
+
+        return back()->with('message', ['success', 'Publicación de nueva oferta exitosa']);
+    }
+
+    public function profileIndex(Request $request) {
+        return;
     }
 }
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Offers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index() {
+        $offers = Offers::withCount('candidates')
+            ->with('business')
+            ->with('businessMeta')
+            ->latest()
+            ->paginate(20);
+
+        return view('home', compact('offers'));
+    }
 }
