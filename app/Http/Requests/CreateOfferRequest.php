e22ff2145621c34e24b53d7141fa9ad5179f587d
cartes <cristiabncartesa@gmail.com>
2020-01-17 09:47:40 -0300

    Validacion formulario creación de puestos

---
+++ app/Http/Requests/CreateOfferRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateOfferRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'description' => 'required|min:15|max:3500',
+            'area' => 'required',
+            'subarea' => 'required',
+            'country' => 'required',
+            'state' => 'required',
+            'city' => 'required',
+            'comune' => 'required',
+            'address' => 'required',
+        ];
+    }
+}
