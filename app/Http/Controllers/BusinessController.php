b2e7d93f780cf8074daf4a48f5d3cefffc33028a
cartes <cristiabncartesa@gmail.com>
2020-01-22 16:20:27 -0300

    Cambié el metodo de creacion de ofertas a un nuevo controlador

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -6,8 +6,6 @@ use App\Business;
 use App\BusinessMeta;
 use App\Http\Requests\BusinessLoginRequest;
 use App\Http\Requests\BusinessProfileRequest;
-use App\Http\Requests\CreateOfferRequest;
-use App\Offers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -30,35 +28,13 @@ class BusinessController extends Controller
                     'role' => 'business'
                 ]);
 
-                return redirect()->route('post.create');
+                return redirect()->route('business.profile');
             } else {
                 return back()->with('message', ['danger', 'Contraseña no coincide, inténtelo de nuevo']);
             }
         } else {
             return back()->with('message', ['warning', 'Email no está registrado como empresa']);
         }
-    }
-
-    public function createOffer(CreateOfferRequest $request)
-    {
-        $offer = new Offers;
-        $business_id = session()->get('id');
-
-        $offer->id_business = $business_id;
-        $offer->title = $request->title;
-        $offer->description = $request->description;
-        $offer->area = $request->area;
-        $offer->sub_area = $request->subarea;
-        $offer->country = $request->country;
-        $offer->state = $request->state;
-        $offer->city = $request->city;
-        $offer->comune = $request->comune;
-        $offer->address = $request->address;
-        $offer->salary = $request->salary;
-
-        $offer->save();
-
-        return back()->with('message', ['success', 'Publicación de nueva oferta exitosa']);
     }
 
     public function profileIndex(Request $request)
@@ -85,10 +61,18 @@ class BusinessController extends Controller
                 'city' => $request->city,
                 'comune' => $request->comune,
                 'phone' => $request->phone,
+                'entry' => $request->entry
             ]);
         Business::where('id', session()->get('id'))
             ->update([
-                'email' => $requet->email
+                'email' => $request->email,
+                'firstname' => $request->userName,
+                'surname' => $request->userName,
+                'position' => $request->userName,
+                'position' => $request->userPosition,
+                'phone' => $request->userPhone,
             ]);
+
+        return back()->with('message', ['success', 'Perfil editado correctamente']);
     }
 }
